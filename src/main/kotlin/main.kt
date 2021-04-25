66f1c280996f042795f8f6c5c7db3ea5b313a8d0
Raphaelamorimalex <67813638+Raphaelamorimalex@users.noreply.github.com>
2021-04-25 13:48:46 -0300

    Initial commit

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,26 @@
+fun primos(numero:Int):Boolean{
+    var j = 2
+    var calculo:Int = (Math.sqrt(numero.toDouble())).toInt()
+    for(j in 2..calculo + 1){
+        if(numero % j == 0){
+            return false
+        }
+    }
+    return true
+}
+
+fun main(args: Array<String>) {
+    //continue a solução
+    val n = readLine()!!.toInt()
+    var x:Int
+    var check:Boolean
+    for (i in 1..n) {
+        x = readLine()!!.toInt()
+        check = primos(x)
+        if((x>=2) && (check == true)){
+            println("Prime")
+        }else{
+            println("Not Prime")
+        }
+    }
+}
